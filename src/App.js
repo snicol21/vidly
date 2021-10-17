fb1c1b9dcaabfeccac1c435d5aabc7ef124da339
HQCATALYST\spencer.nicol <spencer.nicol@healthcatalyst.com>
2021-10-16 18:31:48 -0600

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { Route, Redirect, Switch } from "react-router-dom";
+import NavBar from "./components/navBar";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </>
   );
 }
 
---
+++ src/components/navBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand navbar-light bg-light">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          Vidly
+        </Link>
+        <div className="navbar-nav mr-auto">
+          <NavLink className="nav-item nav-link" to="/movies">
+            Movies
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/customers">
+            Customers
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/rentals">
+            Rentals
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
