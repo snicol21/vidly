0ac8d4ab273b007420025b8602d3a1654145b8eb
HQCATALYST\spencer.nicol <spencer.nicol@healthcatalyst.com>
2021-10-17 18:17:52 -0600

    add register form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Route, Redirect, Switch } from "react-router-dom";
 import NavBar from "./components/navBar";
 import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 import "./App.css";
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/login" component={LoginForm}></Route>
+          <Route path="/register" component={RegisterForm}></Route>
+          <Route path="/movies/:id" component={MovieForm}></Route>
           <Route path="/movies" component={Movies}></Route>
           <Route path="/customers" component={Customers}></Route>
           <Route path="/rentals" component={Rentals}></Route>
